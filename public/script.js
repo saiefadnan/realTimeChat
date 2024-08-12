7f22316cd95c36d96222fed8895fc6b90bd061db
saiefadnan <saiefadnan078@gmail.com>
2024-08-12 16:59:28 +0600

    frontend and backend modification-2

--- public/script.js
+++ public/script.js
@@ -1,49 +1,61 @@
-(function(){
-  function updateStyles() {
-    if (window.innerWidth < 1000) {
-        userDiv.classList.add('userDiv');
-    } else {
-        userDiv.classList.remove('userDiv');
+const url = 'https://realtimechat-7aqr.onrender.com';
+const socket = io(url);
+    document.getElementById('registerButton').addEventListener('click', () => {
+    const email = document.getElementById('emailInput').value;
+    const username = document.getElementById('usernameInput').value;
+    const password = document.getElementById('passwordInput').value;
+    const profilePic = document.getElementById('profilePic');
+    const profile = profilePic.files[0];
+    if (username.trim() && email.trim() && password.trim() && profile) {
+        const reader = new FileReader();
+        reader.onload = () => {
+            const fileData = reader.result.split(',')[1];;
+            socket.emit('register', {email, username ,password, profile: { name: profile.name, data: fileData, type: profile.type }});
+        }
+        reader.readAsDataURL(profile);
+        document.getElementById('emailInput').value='';
+        document.getElementById('usernameInput').value='';
+        document.getElementById('passwordInput').value='';
     }
-}
-if(sessionStorage.getItem('login')==='true'){
-    if (window.socket && window.socket.connected) {
-        console.log('WebSocket is connected.');
-        window.socket.emit('show active-users');
-    } 
+    else if (username.trim() && email.trim() && password.trim()) {
+      socket.emit('register', {email, username ,password, profile:"false"});
+      document.getElementById('emailInput').value='';
+      document.getElementById('usernameInput').value='';
+      document.getElementById('passwordInput').value='';
+    }
     else{
-      const url = 'https://realtimechat-7aqr.onrender.com';
-        window.socket = io();
-        console.log('u are connecting...');
-        window.socket.emit('insert name',{
-            username: sessionStorage.getItem('username'),
-            imageurl: sessionStorage.getItem('imageurl')
-    });
+        console.error('No file selected');
+        addNotify('All fields should be filled!');
     }
-}
-else{
-    console.log('No connection');
-}
+  });
 
-if(window.socket){
-    const socket = window.socket;
-    document.getElementById('sendButton').addEventListener('click', () => {
+  document.getElementById('loginButton').addEventListener('click', () => {
+    const email = document.getElementById('logemailInput').value;
+    const password = document.getElementById('logpasswordInput').value;
+    console.log(email,password);
+    if (email.trim() && password.trim()) {
+      socket.emit('login', email, password);
+      document.getElementById('logemailInput').value='';
+      document.getElementById('logpasswordInput').value='';
+    }
+  });
+
+  document.getElementById('sendButton').addEventListener('click', () => {
     const recipient = document.getElementById('recipientInput').value;
     const message = document.getElementById('messageInput').value;
     if(recipient.trim() && message.trim() && recipient==='public'){
-      addMessageTo(message, new Date(Date.now()).toLocaleString()) ;
       socket.emit('public message', message);
+      addMessageTo(`Me (public): ${message}`) ;
       document.getElementById('messageInput').value = '';
     }
     else if (recipient.trim() && message.trim()) {
-      addMessageTo(message, new Date(Date.now()).toLocaleString()) ;
       socket.emit('private message', { to: recipient, message });
+      addMessageTo(`Me (${recipient}): ${message}`) ;
       document.getElementById('messageInput').value = '';
     }
-    const fileinput = document.getElementById('file-input');
+    const fileinput = document.getElementById('fileInput');
     const file = fileinput.files[0];
     if(file && recipient.trim() && recipient==='public') {
-        document.getElementById('custom-file-upload').style.backgroundColor = '#007bff';
         const reader = new FileReader();
         reader.onload = () => {
             const fileData = reader.result;
@@ -61,10 +73,9 @@ if(window.socket){
             }
             };
             reader.readAsDataURL(file);
-            document.getElementById('file-input').value = '';
+            document.getElementById('fileInput').value = '';
     }
     else if(file && recipient.trim()) {
-        document.getElementById('custom-file-upload').style.backgroundColor = '#007bff';
         const reader = new FileReader();
         reader.onload = () => {
             const fileData = reader.result;
@@ -80,23 +91,19 @@ if(window.socket){
             }
             };
             reader.readAsDataURL(file);
-            document.getElementById('file-input').value = '';
+            document.getElementById('fileInput').value = '';
         }
   });
 
-  //message
   socket.on('private message', ({ from, time, message, profile }) => {
-    const date = new Date(time).toLocaleString();
-    addMessage(from, message, date, profile) ;
+    addMessage(`${from}: ${message}`,profile) ;
   });
 
 
   socket.on('public message', ({ from, time, message, profile  }) => {
-    const date = new Date(time).toLocaleString();
-    addMessage(from, message, date, profile) ;
+    addMessage(`${from} (public): ${message}`,profile) ;
   });
 
-  //image
   socket.on('private image',({from,time,fileData, profile  })=>{
     addImage(from,fileData,profile) ;
   })
@@ -104,8 +111,6 @@ if(window.socket){
   socket.on('public image',({from,time,fileData, profile  })=>{
     addImage(from,fileData,profile) ;
   })
-
-  //video
 
   socket.on('private video',({from,time,fileData, profile  })=>{
     addVideo(from,fileData,profile) ;
@@ -115,17 +120,12 @@ if(window.socket){
     addVideo(from,fileData,profile) ;
   })
 
-  //file
-
   socket.on('private file',({from,time,fileData,fileName, profile  })=>{
     addFile(from,fileData,fileName,profile);
   })
   socket.on('public file',({from,time,fileData,fileName, profile })=>{
     addFile(from,fileData,fileName,profile);
   })
-
-//rest
-
   socket.on('error', ({error}) => {
     addError(`${error}`) ;
   });
@@ -138,42 +138,19 @@ if(window.socket){
     addPicture(picture);
   })
 
-
   socket.on('activeUsers',({activeUsers, profile})=>{
-    console.log('active users....');
     const activeBar = document.getElementById('active');
-    if(!activeUsers.includes('public')){
-        activeUsers.push('public');
-        profile.push('https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&w=600');
-    }
+    activeUsers.push('public');
+    profile.push('https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&w=600');
     activeBar.innerHTML = ''; 
     activeUsers.forEach((name, index) => {
         const userDiv = document.createElement('div');
         const profileDiv = document.createElement('img');
-        const phoneActive = document.getElementById('phoneview-active');
         const profileUrl = profile[index];
-        const activeDiv = document.createElement('div');
-        // const statusDiv = document.createElement('div');
-        // statusDiv.style.width = '10px';
-        // statusDiv.style.height = '10px';
-        // statusDiv.style.borderRadius = '50%';
-        // statusDiv.style.padding = '0';
-        // statusDiv.style.backgroundColor = 'lightgreen';
-        // statusDiv.style.marginLeft = '80%';
-        // statusDiv.style.marginBottom = '1%';
-        // statusDiv.style.display = 'block';
-
-        activeDiv.style.display = 'flex';
-        activeDiv.style.alignItems = 'center';
-        activeDiv.style.justifyContent = 'center';
-        activeDiv.style.backgroundColor = 'lightgreen';
-        activeDiv.style.borderRadius = '50%';
-        activeDiv.style.width = '70px';
-        activeDiv.style.height = '70px';
-
+        console.log(profileUrl);
         userDiv.style.height = '70px';
         userDiv.style.width = '80%';
-        if(name!=='public') userDiv.style.backgroundColor = 'cadetblue';
+        if(name!=='public') userDiv.style.backgroundColor = 'lightgreen';
         else userDiv.style.backgroundColor = 'crimson';
         userDiv.style.display = 'flex';
         userDiv.style.alignItems = 'center';
@@ -181,9 +158,7 @@ if(window.socket){
         userDiv.style.border = '1px solid black'
         userDiv.style.borderRadius = '10px';
         userDiv.style.padding = '0 20px';
-
-        if(sessionStorage.getItem('username')===name)userDiv.textContent = `${name}(me)`;
-        else userDiv.textContent = name;
+        userDiv.textContent = name;
         userDiv.style.marginTop ='5px';
 
         profileDiv.src=profileUrl;
@@ -191,16 +166,9 @@ if(window.socket){
         profileDiv.style.height = '60px';
         profileDiv.style.borderRadius = '50%';
         profileDiv.style.border = '1px soild #ccc';
-        const profileDivClone = profileDiv.cloneNode(true);
-        profileDivClone.style.cursor = 'pointer';
-
-        activeDiv.addEventListener('click',()=>{
-          document.getElementById('recipientInput').value=`${name}`;
-        })
-
         userDiv.addEventListener('mouseover', () => {
-            userDiv.style.backgroundColor = 'rgb(55, 88, 89)';
-            userDiv.style.cursor = 'pointer'; 
+            userDiv.style.backgroundColor = 'cadetblue';
+            userDiv.style.cursor = 'pointer';
         });
         userDiv.addEventListener('click', () => {
             document.getElementById('recipientInput').value=`${name}`;
@@ -209,16 +177,9 @@ if(window.socket){
             if(name!=='public')userDiv.style.backgroundColor = 'lightgreen';
             else userDiv.style.backgroundColor = 'crimson';
         });
-        activeDiv.appendChild(profileDivClone);
-        //activeDiv.appendChild(statusDiv);
-        //profileDivClone.appendChild(statusDiv);
-        phoneActive.style.backgroundColor = 'yellow';
-        phoneActive.appendChild(activeDiv);
         userDiv.appendChild(profileDiv);
         activeBar.appendChild(userDiv);
         activeBar.scrollTop = activeBar.scrollHeight;
-
-      window.addEventListener('resize', updateStyles);
     });
   });
 
@@ -226,33 +187,15 @@ if(window.socket){
 function addImageTo(to, imageData){
     const messagesDiv = document.getElementById('messages');
     const messageElement = document.createElement('div');
-    const modal = document.createElement('div');
     messageElement.style.padding = '8px';
     messageElement.style.margin = '10px';
     messageElement.style.textAlign = 'left';
-    messageElement.style.width = '35%';
-    messageElement.style.marginLeft = '85%';
-    messageElement.innerHTML = `<strong>Me (${to}):</strong><br><img src="${imageData}" alt="Image" style="max-width: 150px; max-width: 150px;">`;
+    messageElement.style.width = '50%';
+    messageElement.style.marginLeft = '70%';
+    messageElement.innerHTML = `<strong>Me (${to}):</strong><br><img src="${imageData}" alt="Image" style="max-width: 200px; max-height: 200px;">`;
     messagesDiv.appendChild(messageElement);
     messagesDiv.scrollTop = messagesDiv.scrollHeight;
-    messageElement.querySelector('img').style.cursor = 'pointer';
-    
-    messageElement.querySelector('img').addEventListener('click',()=>{
-        if(messageElement.querySelector('img').style.width=='200px'){
-            console.log('asda');
-            modal.style.position = 'fixed';
-            modal.style.top = '50%';
-            modal.style.left = '50%';
-            modal.style.transform = 'translate(-50%, -50%)';
-            modal.style.width = '100%';
-            modal.style.height = '100%';
-            modal.style.backgroundColor = '#ccc';
-            modal.style.zIndex = '1000';
-        }
-        else{
-            modal.style.display = 'none';
-        }
-    })
+    //at ${new Date(time).toLocaleTimeString()}
 }
 
 
@@ -261,7 +204,7 @@ function addImage(from, imageData,profile){
     const messageElement = document.createElement('div');
     const profileDiv = document.createElement('img');
     const messageContainer = document.createElement('div');
-    messageElement.innerHTML = `<strong>${from}:</strong><br><img src="${imageData}" alt="Image" style="max-width: 215px; max-height: 215px;">`;
+    messageElement.innerHTML = `<strong>${from}:</strong><br><img src="${imageData}" alt="Image" style="max-width: 200px; max-height: 200px;">`;
 
     profileDiv.src=profile;
     profileDiv.style.width = '70px';
@@ -279,6 +222,7 @@ function addImage(from, imageData,profile){
     messageElement.style.width= '95%';
     messagesDiv.appendChild(messageContainer);
     messagesDiv.scrollTop = messagesDiv.scrollHeight;
+    //at ${new Date(time).toLocaleTimeString()}
 }
 
 
@@ -289,7 +233,7 @@ function addVideoTo(to, videoData) {
     messageElement.style.margin = '10px';
     messageElement.style.textAlign = 'left';
     messageElement.style.width = '50%';
-    messageElement.style.marginLeft = '85%';
+    messageElement.style.marginLeft = '70%';
     messageElement.innerHTML = `<strong>Me (${to}):</strong><br><video controls style="max-width: 200px; max-height: 200px;">
                             <source src="${videoData}" type="video/mp4">
                             Your browser does not support the video tag.
@@ -358,74 +302,56 @@ function addFileTo(to, fileData, fileName) {
     messageElement.style.margin = '10px';
     messageElement.style.textAlign = 'left';
     messageElement.style.width = '50%';
-    messageElement.style.marginLeft = '85%';
+    messageElement.style.marginLeft = '70%';
     messageElement.innerHTML = `<strong>Me (${to}):</strong><br><a href="${fileData}" download="${fileName}">${fileName}</a>`;
     messagesDiv.appendChild(messageElement);
     messagesDiv.scrollTop = messagesDiv.scrollHeight;
 }
 
 
-function addMessage(from, message, time ,profile) {
+function addMessage(message, profile) {
     const messagesDiv = document.getElementById('messages');
     const messageElement = document.createElement('div');
     const profileDiv = document.createElement('img');
-    const timeDiv = document.createElement('h6');
-    const nameDiv = document.createElement('h6');
-    const time_nameDiv = document.createElement('div');
     const messageContainer = document.createElement('div');
-    const finalContainer = document.createElement('div');
-
     messageElement.textContent = message;
-    messageElement.className="message-receive";
-    timeDiv.textContent=time;
-    timeDiv.style.color = '#ccc';
-    nameDiv.textContent=from;
-    nameDiv.style.color = '#ccc';
+    messageElement.style.backgroundColor = 'lightblue';
+    messageElement.style.borderRadius = '10px';
+    messageElement.style.margin = '10px';
+    messageElement.style.textAlign = 'left';
+    messageElement.style.padding = '10px';
 
-    time_nameDiv.appendChild(nameDiv);
-    time_nameDiv.appendChild(timeDiv);
-    time_nameDiv.className = 'time-name-container';
-
-    
     profileDiv.src=profile;
-    profileDiv.className = 'receiver-profile-container';
+    profileDiv.style.width = '70px';
+    profileDiv.style.height = '70px';
+    profileDiv.style.borderRadius = '50%';
+    profileDiv.style.border = '1px soild #ccc';
+    profileDiv.style.padding = '10px';
 
-    messageContainer.className = 'message-container';
+    messageContainer.style.width = '50%';
     messageContainer.appendChild(profileDiv);
     messageContainer.appendChild(messageElement);
-
-    finalContainer.className ='final-container';
-    finalContainer.appendChild(time_nameDiv);
-    finalContainer.appendChild(messageContainer);
-
-    messagesDiv.appendChild(finalContainer);
+    messageContainer.style.display = 'flex';
+    messageContainer.style.justifyContent = 'center';
+    messageContainer.style.alignItems = 'center';
+    messageElement.style.width= '95%';
+    messagesDiv.appendChild(messageContainer);
     messagesDiv.scrollTop = messagesDiv.scrollHeight;
-
   }
   
-  function addMessageTo(message, time) {
-    const messagesDiv = document.getElementById('messages');
-    const messageElement = document.createElement('div');
-    const timeDiv = document.createElement('h6');
-    const messageContainer = document.createElement('div');
-    const finalContainer = document.createElement('div');
-
-    messageElement.textContent = message;
-    messageElement.className="message-send";
-    timeDiv.textContent=time;
-    timeDiv.className = 'time-container';
-    timeDiv.style.color = '#ccc';
-
-    messageContainer.className = 'message-container';
-    messageContainer.appendChild(messageElement);
-
-    finalContainer.className ='final-container';
-    finalContainer.appendChild(timeDiv);
-    finalContainer.style.marginLeft = '50%'
-    finalContainer.appendChild(messageContainer);
-
-    messagesDiv.appendChild(finalContainer);
-    messagesDiv.scrollTop = messagesDiv.scrollHeight;
+  function addMessageTo(message) {
+      const messagesDiv = document.getElementById('messages');
+      const messageElement = document.createElement('div');
+      messageElement.textContent = message;
+      messageElement.style.backgroundColor = 'rgb(38, 158, 198)';
+      messageElement.style.borderRadius = '10px';
+      messageElement.style.padding = '10px';
+      messageElement.style.margin = '10px';
+      messageElement.style.textAlign = 'left';
+      messageElement.style.width = '50%';
+      messageElement.style.marginLeft = '45%';
+      messagesDiv.appendChild(messageElement);
+      messagesDiv.scrollTop = messagesDiv.scrollHeight;
   }
 function addNotify(message) {
     const notifyDiv = document.getElementById('info');
@@ -459,9 +385,3 @@ function addError(message) {
         profileDiv.src=picture;
     }
   }
-
-}})();
-
-document.getElementById('file-input').addEventListener('change',()=>{
-        document.getElementById('custom-file-upload').style.backgroundColor = 'crimson'
-})
