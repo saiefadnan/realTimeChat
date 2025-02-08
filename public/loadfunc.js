4155f74b384f34fd9f94f200b54537d8fdeda0a4
saiefadnan <saiefadnan078@gmail.com>
2025-02-08 21:39:50 +0600

    frontend modification, bugs removed-1.1

--- public/loadfunc.js
+++ public/loadfunc.js
@@ -83,3 +83,34 @@ export function toggleColor(element){
     links.forEach((link)=>{link.classList.remove('clicked')});
     element.classList.toggle('clicked');
 }
+
+export function handleLogout(){
+    Cookies.remove('token');
+        window.loadPage('login.html','login');
+        var logouts = document.getElementsByClassName('nav-logout');
+        for (let logout of logouts) {
+            logout.style.display = 'none';
+        }
+
+        var signups = document.getElementsByClassName('nav-signup');
+        for (let signup of signups) {
+            signup.style.display = 'block';
+        }
+
+        var logins = document.getElementsByClassName('nav-login');
+        for (let login of logins) {
+            login.style.display = 'block';
+        }
+        var chats = document.getElementsByClassName('nav-chat');
+        for (let chat of chats) {
+            chat.style.display = 'none';
+        }
+        var rooms = document.getElementsByClassName('nav-room');
+        for (let room of rooms) {
+            room.style.display = 'none';
+        }
+        var profileDivs = document.getElementsByClassName('circle responsive-img');
+        Array.from(profileDivs).forEach((profileDiv)=>{
+            profileDiv.src="https://gifdb.com/images/high/eren-yeager-blowing-hair-o63aaatimhxaojbu.gif";
+        })
+}
